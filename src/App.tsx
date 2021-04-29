112c7a33dfe88c85af1f22009bb38d2bbf5c3192
Nachanok <48919210+nachanok-work@users.noreply.github.com>
2021-04-30 01:36:14 +0700

    finish add/edit modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@
 //import KalendarApp from "./App";
 import "./App.css";
 import MyModal from "./MyModal";
+import AddModal from "./addModal";
+import EditModal from "./EditModal";
+import ExportModal from "./export";
 
 const App = () => {
   return (
     <div className="App">
-      <MyModal message="hello from app" />
+      <ExportModal message="hello from app" />
     </div>
   );
 };
---
+++ src/export.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import Button from "./Component/Button";
+import Form from "./Component/Form";
+import Input from "./Component/Input";
+import { Modal } from "./Component/Modal";
+import { useKeyValue } from "./helper";
+
+const mykeys = ["id", "password"];
+
+const useMyModalForm = () => {
+  const form = useKeyValue(mykeys);
+
+  const enabledSubmit = () => {
+    const idCheck = form.getValue("id").toString().length >= 8;
+    const passwordCheck = form.getValue("password").toString().length >= 8;
+    return idCheck && passwordCheck;
+  };
+
+  return { form, enabledSubmit };
+};
+
+const ExportModal = ({ message /* test props */ }: { message: string }) => {
+  const [showModal, setShowModal] = useState(false);
+  const { form, enabledSubmit } = useMyModalForm();
+
+  const onSubmit = async () => {
+    console.log(form.getValue("id").toString(), form.getValue("password").toString());
+    setShowModal(false);
+  };
+
+  const toggleModal = (show: boolean) => {
+    setShowModal(show);
+  };
+
+  return (
+    <div className="pt-5">
+      <div>
+        <Button
+          color="blue3"
+          onClick={() => {
+            setShowModal(true);
+          }}>
+          Open Modal
+        </Button>
+      </div>
+      {
+        <Modal
+          close={() => {
+            /* try not to directly pass a setState function. ! 
+          Instead, we pass a custom setState function.
+        */
+            toggleModal(false);
+          }}
+          title="Preview"
+          show={showModal}
+          disabledDone={!enabledSubmit()}
+          onDone={onSubmit}>
+          <div>
+            <div>{message}</div>
+            <div>
+              <Form onSubmit={onSubmit} enabledSubmit={enabledSubmit()}>
+                <div className="pt-2">
+                  <div className="pt-2">
+                    
+                  </div>
+                  <div className="pt-2">
+                    <Input
+                      type="text"
+                      icon="far fa-file-pdf"
+                      placeholder="Location"
+                      value={form.getValue("id") as string}
+                      onChange={(value) => {
+                        form.updateValue("id", value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label">All day</label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label">Start</label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label">End</label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label">Repeat</label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+                    <label className="form-check-label">Alert</label>
+                  </div>
+                  <div className="pt-2">
+                    <Input
+                      type="text"
+                      icon=""
+                      placeholder="Note"
+                      value={form.getValue("id") as string}
+                      onChange={(value) => {
+                        form.updateValue("id", value);
+                      }}
+                    />
+                  </div>
+                  <div>
+                    {/* submit button required for submitting through enter/return key */}
+                    <Button type="submit"></Button>
+                  </div>
+                </div>
+              </Form>
+            </div>
+          </div>
+        </Modal>
+      }
+    </div>
+  );
+};
+
+export default ExportModal;
